c591fbb1e6813ac0c5d01b73c29e83b891740d6b
seungku.choi <seungku.choi@mobis.co.kr>
2023-11-27 14:00:53 +0900

    Modify TimerUI

--- datastore-with-hilt/app/src/main/java/com/example/datastorewithhilt/TimerScreen.kt
+++ datastore-with-hilt/app/src/main/java/com/example/datastorewithhilt/TimerScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.ui.Modifier
@@ -20,20 +21,28 @@ fun TimerScreen(
         Column(modifier = Modifier.padding(it)) {
             Text(text = "Time remaining: ${time.value}")
 
-            Button(onClick = { viewModel.startTimer(60) }) {
-                Text("Start(60s)")
-            }
+            Row(
+                modifier = Modifier
+            ) {
+                Button(
+                    onClick = {
+                        viewModel.startTimer(60)
+                    }
+                ) {
+                    Text("Start(60s)")
+                }
 
-            Button(onClick = { viewModel.pauseTimer() }) {
-                Text("Pause")
-            }
+                Button(onClick = { viewModel.pauseTimer() }) {
+                    Text("Pause")
+                }
 
-            Button(onClick = { viewModel.resumeTimer() }) {
-                Text("Resume")
-            }
+                Button(onClick = { viewModel.resumeTimer() }) {
+                    Text("Resume")
+                }
 
-            Button(onClick = { viewModel.stopTimer() }) {
-                Text("Stop")
+                Button(onClick = { viewModel.stopTimer() }) {
+                    Text("Stop")
+                }
             }
         }
     }
